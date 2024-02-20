eab540ebad927043990aaee74d85aa042a0ef416
Juan <Juan@DESKTOP-A259E23>
2024-02-20 10:14:28 +0100

    mi primer commit :(

---
+++ src/main/java/com/mycompany/minicalculadora/miniCalculadora.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.minicalculadora;
+
+/**
+ *
+ * @author Juan
+ */
+
+public class miniCalculadora {
+
+    public static double valorAbsoluto(double in) {
+        return Math.abs(in);
+    }
+
+    public static double raizCuadrada(double in) {
+        return Math.sqrt(in);
+    }
+    
+  
+}
