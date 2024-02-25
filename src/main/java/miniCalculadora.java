e1666dd4defb2899c03cf44d7f2e44eea38f8095
Merilax <merilaxreloaded@gmail.com>
2024-02-25 15:49:12 +0100

    Fix package discrepancies

---
+++ src/main/java/miniCalculadora.java
@@ -0,0 +1,23 @@
+/**
+ * @author Juan
+ */
+
+public class miniCalculadora {
+
+    public static double valorAbsoluto(double in) {
+        return Math.abs(in);
+    }
+
+    public static double raizCuadrada(double in) {
+        return Math.sqrt(in);
+    }
+    
+    /**
+     * @param a double
+     * @param b double
+     * @return double addition of a and b
+     */
+    public static double add(double a, double b) {
+        return a + b;
+    }
+}
